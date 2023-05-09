d1b2859f6f9db172e3ba02cf8f132914ea1651af
Ryan Maber <ryan@ryanmaber.com>
2023-05-09 19:45:23 +0100

    Reformat all terraform

--- infrastructure/bucket/variables.tf
+++ infrastructure/bucket/variables.tf
@@ -1,4 +1,4 @@
 variable "environment" {
-    type    = string
-    default = "dev"
+  type    = string
+  default = "dev"
 }
--- infrastructure/queue/main.tf
+++ infrastructure/queue/main.tf
@@ -1,15 +1,15 @@
 resource "aws_sqs_queue" "coverage_analysis_queue" {
-    name = format("coverage-analysis-queue-%s", var.environment)
+  name = format("coverage-analysis-queue-%s", var.environment)
 
-    redrive_policy = jsonencode({
-        deadLetterTargetArn = aws_sqs_queue.coverage_analysis_deadletter_queue.arn
-        maxReceiveCount     = 1
-    })
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.coverage_analysis_deadletter_queue.arn
+    maxReceiveCount     = 1
+  })
 }
 
 resource "aws_sqs_queue" "coverage_analysis_deadletter_queue" {
-    name = format("coverage-analysis-deadletter-queue-%s", var.environment)
+  name = format("coverage-analysis-deadletter-queue-%s", var.environment)
 
-    # Store deadletter messages for 2 weeks
-    message_retention_seconds = 1209600
+  # Store deadletter messages for 2 weeks
+  message_retention_seconds = 1209600
 }
