f7232c9157450b0fded0432969b3c63619ce1ea6
Ryan Maber <ryan@ryanmaber.com>
2023-05-30 20:50:15 +0100

    Refactoring tests

--- services/analyse/tests/Service/QueryServiceTest.php
+++ services/analyse/tests/Service/QueryServiceTest.php
@@ -5,9 +5,12 @@ namespace App\Tests\Service;
 use App\Client\BigQueryClient;
 use App\Exception\QueryException;
 use App\Model\QueryResult\IntegerQueryResult;
+use App\Model\QueryResult\QueryResultInterface;
 use App\Model\QueryResult\TotalCoverageQueryResult;
+use App\Model\QueryResult\TotalTagCoverageQueryResult;
 use App\Model\Upload;
 use App\Query\TotalCoverageQuery;
+use App\Query\TotalTagCoverageQuery;
 use App\Query\TotalUploadsQuery;
 use App\Service\QueryService;
 use App\Tests\Mock\Factory\MockQueryFactory;
@@ -19,7 +22,7 @@ use PHPUnit\Framework\TestCase;
 class QueryServiceTest extends TestCase
 {
     #[DataProvider('queryDataProvider')]
-    public function testRunQuery(string $query, ?string $expectedException): void
+    public function testRunQuery(string $query, QueryResultInterface $queryResult): void
     {
         $mockBigQueryService = $this->createMock(BigQueryClient::class);
 
@@ -29,41 +32,80 @@ class QueryServiceTest extends TestCase
                 MockQueryFactory::createMock(
                     $this,
                     TotalCoverageQuery::class,
-                    TotalCoverageQuery::class === $query ? 'mock-query' : null,
-                    TotalCoverageQueryResult::from([
-                        'lines' => 6,
-                        'covered' => 1,
-                        'partial' => 2,
-                        'uncovered' => 3,
-                        'coveragePercentage' => 0
-                    ])
+                    TotalCoverageQuery::class,
+                    TotalCoverageQuery::class === $query ?
+                        $queryResult : $this->createMock(TotalCoverageQueryResult::class)
                 ),
                 MockQueryFactory::createMock(
                     $this,
                     TotalUploadsQuery::class,
-                    TotalUploadsQuery::class === $query ? 'mock-query' : null,
-                    IntegerQueryResult::from(99)
+                    TotalUploadsQuery::class,
+                    TotalUploadsQuery::class === $query ? $queryResult : $this->createMock(IntegerQueryResult::class)
+                ),
+                MockQueryFactory::createMock(
+                    $this,
+                    TotalTagCoverageQuery::class,
+                    TotalTagCoverageQuery::class,
+                    TotalTagCoverageQuery::class === $query ?
+                        $queryResult : $this->createMock(TotalTagCoverageQueryResult::class)
+                )
+            ]
+        );
+
+        $mockQueryJobConfiguration = $this->createMock(QueryJobConfiguration::class);
+
+        $mockBigQueryService->expects($this->once())
+            ->method('query')
+            ->with($query)
+            ->willReturn($mockQueryJobConfiguration);
+
+        $mockBigQueryService->expects($this->once())
+            ->method('runQuery')
+            ->with($mockQueryJobConfiguration)
+            ->willReturn($this->createMock(QueryResults::class));
+
+        $result = $queryService->runQuery($query, $this->createMock(Upload::class));
+
+        $this->assertEquals($queryResult, $result);
+    }
+
+    public function testRunInvalidQuery(): void
+    {
+        $mockBigQueryService = $this->createMock(BigQueryClient::class);
+
+        $queryService = new QueryService(
+            $mockBigQueryService,
+            [
+                MockQueryFactory::createMock(
+                    $this,
+                    TotalCoverageQuery::class,
+                    null,
+                    $this->createMock(TotalCoverageQueryResult::class)
+                ),
+                MockQueryFactory::createMock(
+                    $this,
+                    TotalUploadsQuery::class,
+                    null,
+                    $this->createMock(IntegerQueryResult::class)
                 ),
             ]
         );
 
         $mockQueryJobConfiguration = $this->createMock(QueryJobConfiguration::class);
 
-        $mockBigQueryService->expects(!$expectedException ? $this->once() : $this->never())
+        $mockBigQueryService->expects($this->never())
             ->method('query')
             ->with('mock-query')
             ->willReturn($mockQueryJobConfiguration);
 
-        $mockBigQueryService->expects(!$expectedException ? $this->once() : $this->never())
+        $mockBigQueryService->expects($this->never())
             ->method('runQuery')
             ->with($mockQueryJobConfiguration)
             ->willReturn($this->createMock(QueryResults::class));
 
-        if ($expectedException) {
-            $this->expectException($expectedException);
-        }
+        $this->expectException(QueryException::class);
 
-        $queryService->runQuery($query, $this->createMock(Upload::class));
+        $queryService->runQuery('invalid-query', $this->createMock(Upload::class));
     }
 
     public static function queryDataProvider(): array
@@ -71,16 +113,18 @@ class QueryServiceTest extends TestCase
         return [
             'Total commit coverage query' => [
                 TotalCoverageQuery::class,
-                null
+                TotalCoverageQueryResult::from([
+                    'lines' => 6,
+                    'covered' => 1,
+                    'partial' => 2,
+                    'uncovered' => 3,
+                    'coveragePercentage' => 0
+                ])
             ],
             'Total commit uploads query' => [
                 TotalUploadsQuery::class,
-                null
+                IntegerQueryResult::from(99)
             ],
-            'Invalid query' => [
-                'invalid-query',
-                QueryException::class
-            ]
         ];
     }
 }
