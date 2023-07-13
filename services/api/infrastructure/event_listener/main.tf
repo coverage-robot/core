368fc5d409c4886ec61b81b5c6bb78ac9c0c9d4c
Ryan Maber <ryan@ryanmaber.com>
2023-07-14 00:33:34 +0100

    Switch layer applied to lambda

--- services/api/infrastructure/event_listener/main.tf
+++ services/api/infrastructure/event_listener/main.tf
@@ -1,6 +1,5 @@
 locals {
   bref_layers           = jsondecode(file("${path.module}/../../vendor/bref/bref/layers.json"))
-  bref_extension_layers = jsondecode(file("${path.module}/../../vendor/bref/extra-php-extensions/layers.json"))
 }
 
 data "terraform_remote_state" "core" {
@@ -72,12 +71,6 @@ resource "aws_lambda_function" "events" {
       var.region,
       var.php_version,
       local.bref_layers["arm-${var.php_version}"][var.region]
-    ),
-    format(
-      "arn:aws:lambda:%s:403367587399:layer:gd-%s:%s",
-      var.region,
-      var.php_version,
-      local.bref_extension_layers["gd-${var.php_version}"][var.region]
     )
   ]
 
