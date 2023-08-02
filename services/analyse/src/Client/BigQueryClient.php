96b8712b16acc9781c116ad27948ec6052f6ac92
Ryan Maber <ryan@ryanmaber.com>
2023-08-02 17:40:15 +0100

    Fix tests

--- services/analyse/src/Client/BigQueryClient.php
+++ services/analyse/src/Client/BigQueryClient.php
@@ -10,6 +10,12 @@ class BigQueryClient extends \Google\Cloud\BigQuery\BigQueryClient
 
     public function __construct(array $config = [])
     {
+        if (!file_exists(self::SERVICE_ACCOUNT_KEY)) {
+            // In environments where theres no service account key (e.g. in CI, or test environments)
+            // we should just use the default constructor and not provide the file.
+            parent::__construct($config);
+        }
+        
         parent::__construct(
             [
                 'projectId' => $_ENV['BIGQUERY_PROJECT'],
