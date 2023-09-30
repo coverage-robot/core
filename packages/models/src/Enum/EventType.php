6e4149df693f567b8aa72a17377f10f12a97bb73
Ryan Maber <ryan@ryanmaber.com>
2023-10-01 00:32:11 +0100

    Ensure mapping is correct for event discriminator
    Signed-off-by: Ryan Maber <ryan@ryanmaber.com>

--- packages/models/src/Enum/EventType.php
+++ packages/models/src/Enum/EventType.php
@@ -4,6 +4,8 @@ namespace Packages\Models\Enum;
 
 enum EventType: string
 {
+    case PIPELINE_STARTED = 'PIPELINE_STARTED';
+
     case PIPELINE_COMPLETE = 'PIPELINE_COMPLETE';
 
     case UPLOAD = 'UPLOAD';
