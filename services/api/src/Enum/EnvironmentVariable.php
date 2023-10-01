b8113a3c9d18905ca13e68d11c7788963b20d633
Ryan Maber <ryan@ryanmaber.com>
2023-10-01 01:13:34 +0100

    Ignore webhooks for check runs created by us
    Signed-off-by: Ryan Maber <ryan@ryanmaber.com>

--- services/api/src/Enum/EnvironmentVariable.php
+++ services/api/src/Enum/EnvironmentVariable.php
@@ -5,6 +5,7 @@ namespace App\Enum;
 enum EnvironmentVariable: string
 {
     case WEBHOOK_QUEUE = 'WEBHOOK_QUEUE';
+    case GITHUB_APP_ID = 'GITHUB_APP_ID';
     case WEBHOOK_SECRET = 'WEBHOOK_SECRET';
     case EVENT_BUS = 'EVENT_BUS';
 }
