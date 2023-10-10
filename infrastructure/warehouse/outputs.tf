c776431c99f14a83b81ebd981c091610b203e9be
Ryan Maber <ryan@ryanmaber.com>
2023-10-10 22:08:17 +0100

    Formatting
    Signed-off-by: Ryan Maber <ryan@ryanmaber.com>

--- infrastructure/warehouse/outputs.tf
+++ infrastructure/warehouse/outputs.tf
@@ -1,11 +1,11 @@
 output "environment_dataset" {
-    value = google_bigquery_dataset.environment_dataset
+  value = google_bigquery_dataset.environment_dataset
 }
 
 output "line_coverage_table" {
-    value = google_bigquery_table.line_coverage
+  value = google_bigquery_table.line_coverage
 }
 
 output "upload_table" {
-    value = google_bigquery_table.upload
+  value = google_bigquery_table.upload
 }
