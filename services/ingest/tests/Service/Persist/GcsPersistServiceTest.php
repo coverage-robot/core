0904eb6dc2c5273c52f65a8927f8c5eb84849eea
Ryan Maber <ryan@ryanmaber.com>
2023-10-10 22:25:22 +0100

    Tests
    Signed-off-by: Ryan Maber <ryan@ryanmaber.com>

--- services/ingest/tests/Service/Persist/GcsPersistServiceTest.php
+++ services/ingest/tests/Service/Persist/GcsPersistServiceTest.php
@@ -9,6 +9,7 @@ use App\Service\BigQueryMetadataBuilderService;
 use App\Service\Persist\GcsPersistService;
 use App\Tests\Mock\Factory\MockEnvironmentServiceFactory;
 use Google\Cloud\BigQuery\Dataset;
+use Google\Cloud\BigQuery\InsertResponse;
 use Google\Cloud\BigQuery\Job;
 use Google\Cloud\BigQuery\LoadJobConfiguration;
 use Google\Cloud\BigQuery\Table;
@@ -26,7 +27,89 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class GcsPersistServiceTest extends KernelTestCase
 {
-    public function testPersist(): void
+    public function testPersistSuccessfully(): void
+    {
+        $mockBucket = $this->createMock(Bucket::class);
+        $mockStorageObject = $this->createMock(StorageObject::class);
+
+        $mockGcsClient = $this->createMock(GoogleCloudStorageClient::class);
+        $mockGcsClient->expects($this->once())
+            ->method('bucket')
+            ->with(
+                sprintf(
+                    GcsPersistService::OUTPUT_BUCKET,
+                    Environment::DEVELOPMENT->value
+                )
+            )
+            ->willReturn($mockBucket);
+
+        $mockBucket->expects($this->once())
+            ->method('upload')
+            ->willReturn($mockStorageObject);
+
+        $mockDataset = $this->createMock(Dataset::class);
+        $mockTable = $this->createMock(Table::class);
+
+        $mockDataset->expects($this->exactly(2))
+            ->method('table')
+            ->willReturn($mockTable);
+        $mockTable->expects($this->once())
+            ->method('loadFromStorage')
+            ->with($mockStorageObject)
+            ->willReturn($this->createMock(LoadJobConfiguration::class));
+        $mockTable->expects($this->once())
+            ->method('insertRow')
+            ->willReturn(new InsertResponse(['insertErrors' => []], []));
+
+        $mockBigQueryClient = $this->createMock(BigQueryClient::class);
+        $mockBigQueryClient->expects($this->exactly(2))
+            ->method('getEnvironmentDataset')
+            ->willReturn($mockDataset);
+
+        $mockJob = $this->createMock(Job::class);
+        $mockJob->method('id')
+            ->willReturn('mock-job-id');
+        $mockJob->expects($this->once())
+            ->method('isComplete')
+            ->willReturn(true);
+
+        $mockBigQueryClient->expects($this->once())
+            ->method('runJob')
+            ->willReturn($mockJob);
+
+        $gcsPersistService = new GcsPersistService(
+            $mockGcsClient,
+            $mockBigQueryClient,
+            $this->getContainer()->get(BigQueryMetadataBuilderService::class),
+            MockEnvironmentServiceFactory::getMock(
+                $this,
+                Environment::DEVELOPMENT,
+                [
+                    EnvironmentVariable::BIGQUERY_LINE_COVERAGE_TABLE->value => 'mock-line-coverage-table',
+                    EnvironmentVariable::BIGQUERY_UPLOAD_TABLE->value => 'mock-upload-table'
+                ]
+            ),
+            new NullLogger()
+        );
+
+        $gcsPersistService->persist(
+            new Upload(
+                Uuid::uuid4()->toString(),
+                Provider::GITHUB,
+                '',
+                '',
+                '',
+                [],
+                'mock-branch-reference',
+                'project/root',
+                1,
+                new Tag('mock-tag', '')
+            ),
+            new Coverage(CoverageFormat::LCOV, 'mock/project/root')
+        );
+    }
+
+    public function testPersistFailsLineCoverageUpload(): void
     {
         $mockBucket = $this->createMock(Bucket::class);
         $mockStorageObject = $this->createMock(StorageObject::class);
@@ -56,6 +139,8 @@ class GcsPersistServiceTest extends KernelTestCase
             ->method('loadFromStorage')
             ->with($mockStorageObject)
             ->willReturn($this->createMock(LoadJobConfiguration::class));
+        $mockTable->expects($this->never())
+            ->method('insertRow');
 
         $mockBigQueryClient = $this->createMock(BigQueryClient::class);
         $mockBigQueryClient->expects($this->once())
@@ -68,6 +153,8 @@ class GcsPersistServiceTest extends KernelTestCase
         $mockJob->expects($this->once())
             ->method('isComplete')
             ->willReturn(true);
+        $mockJob->method('info')
+            ->willReturn(['status' => ['errorResult' => ['message' => 'mock-error-message']]]);
 
         $mockBigQueryClient->expects($this->once())
             ->method('runJob')
@@ -81,11 +168,12 @@ class GcsPersistServiceTest extends KernelTestCase
                 $this,
                 Environment::DEVELOPMENT,
                 [
-                    EnvironmentVariable::BIGQUERY_LINE_COVERAGE_TABLE->value => 'mock-table'
+                    EnvironmentVariable::BIGQUERY_LINE_COVERAGE_TABLE->value => 'mock-line-coverage-table'
                 ]
             ),
             new NullLogger()
         );
+
         $gcsPersistService->persist(
             new Upload(
                 Uuid::uuid4()->toString(),
