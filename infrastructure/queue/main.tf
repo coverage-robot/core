e96eeb62b3715d37cc61fb73eb245b7a10b4cfe4
Ryan Maber <ryan@ryanmaber.com>
2023-10-16 19:22:41 +0100

    Add a queue delay to webhooks so github can has time to start new jobs
    Signed-off-by: Ryan Maber <ryan@ryanmaber.com>

--- infrastructure/queue/main.tf
+++ infrastructure/queue/main.tf
@@ -14,4 +14,10 @@ resource "aws_sqs_queue" "webhooks_queue" {
   visibility_timeout_seconds  = 120
   deduplication_scope         = "messageGroup"
   fifo_throughput_limit       = "perMessageGroupId"
+
+  # Add a small (5 second) delay between webhooks received from providers and analysis on
+  # coverage starting. This gives a short period of time for providers to start more jobs
+  # for the same commit. The queue is a first-in first-out, so the delay cannot be added
+  # on a per-message basis.
+  delay_seconds = 5
 }
