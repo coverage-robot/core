0e5532e0c5aacc819359eabe915bd7245845021b
Ryan Maber <ryan@ryanmaber.com>
2023-10-16 23:29:18 +0100

    Update type
    Signed-off-by: Ryan Maber <ryan@ryanmaber.com>

--- packages/models/src/Model/Event/JobStateChange.php
+++ packages/models/src/Model/Event/JobStateChange.php
@@ -21,7 +21,7 @@ class JobStateChange implements EventInterface
         private readonly string $ref,
         private readonly string $commit,
         private readonly string|int|null $pullRequest,
-        private readonly string $externalId,
+        private readonly string|int $externalId,
         private readonly int $index,
         private readonly JobState $state,
         private readonly bool $initialState,
