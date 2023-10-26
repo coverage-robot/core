330237f9796899b3aced446572a70807fd653dbc
Ryan Maber <ryan@ryanmaber.com>
2023-10-26 23:56:31 +0100

    Preserve memory by using enums for discrimination of query results as opposed to the FQCN
    Signed-off-by: Ryan Maber <ryan@ryanmaber.com>

---
+++ services/analyse/src/Enum/QueryResult.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Enum;
+
+enum QueryResult: string
+{
+    /**
+     * A collection of commits from the data warehouse.
+     */
+    case COMMIT_COLLECTION = "COMMIT_COLLECTION";
+
+    /**
+     * A singular commit from the data warehouse.
+     */
+    case COMMIT = "COMMIT";
+
+    /**
+     * The total coverage for a commit.
+     */
+    case COVERAGE = "COVERAGE";
+
+    /**
+     * A collection of coverage data, split by file.
+     */
+    case FILE_COVERAGE_COLLECTION = "FILE_COVERAGE_COLLECTION";
+
+    /**
+     * A singular file's coverage data.
+     */
+    case FILE_COVERAGE = "FILE_COVERAGE";
+
+    /**
+     * A collection of coverage data, split by line.
+     */
+    case LINE_COVERAGE_COLLECTION = "LINE_COVERAGE_COLLECTION";
+
+    /**
+     * A singular line's coverage data.
+     */
+    case LINE_COVERAGE = "LINE_COVERAGE";
+
+    /**
+     * A collection of coverage data, split by tag.
+     */
+    case TAG_COVERAGE_COLLECTION = "TAG_COVERAGE_COLLECTION";
+
+    /**
+     * A singular tag's coverage data.
+     */
+    case TAG_COVERAGE = "TAG_COVERAGE";
+
+    /**
+     * Information about the uploads for commits in the data warehouse.
+     */
+    case TOTAL_UPLOADS = "TOTAL_UPLOADS";
+}
--- services/analyse/src/Query/Result/QueryResultInterface.php
+++ services/analyse/src/Query/Result/QueryResultInterface.php
@@ -2,21 +2,22 @@
 
 namespace App\Query\Result;
 
+use App\Enum\QueryResult;
 use Symfony\Component\Serializer\Annotation\DiscriminatorMap;
 
 #[DiscriminatorMap(
     'type',
     [
-        CommitCollectionQueryResult::class => CommitCollectionQueryResult::class,
-        CommitQueryResult::class => CommitQueryResult::class,
-        CoverageQueryResult::class => CoverageQueryResult::class,
-        FileCoverageCollectionQueryResult::class => FileCoverageCollectionQueryResult::class,
-        FileCoverageQueryResult::class => FileCoverageQueryResult::class,
-        LineCoverageCollectionQueryResult::class => LineCoverageCollectionQueryResult::class,
-        LineCoverageQueryResult::class => LineCoverageQueryResult::class,
-        TagCoverageCollectionQueryResult::class => TagCoverageCollectionQueryResult::class,
-        TagCoverageQueryResult::class => TagCoverageQueryResult::class,
-        TotalUploadsQueryResult::class => TotalUploadsQueryResult::class,
+        QueryResult::COMMIT_COLLECTION->value => CommitCollectionQueryResult::class,
+        QueryResult::COMMIT->value => CommitQueryResult::class,
+        QueryResult::COVERAGE->value => CoverageQueryResult::class,
+        QueryResult::FILE_COVERAGE_COLLECTION->value => FileCoverageCollectionQueryResult::class,
+        QueryResult::FILE_COVERAGE->value => FileCoverageQueryResult::class,
+        QueryResult::LINE_COVERAGE_COLLECTION->value => LineCoverageCollectionQueryResult::class,
+        QueryResult::LINE_COVERAGE->value => LineCoverageQueryResult::class,
+        QueryResult::TAG_COVERAGE_COLLECTION->value => TagCoverageCollectionQueryResult::class,
+        QueryResult::TAG_COVERAGE->value => TagCoverageQueryResult::class,
+        QueryResult::TOTAL_UPLOADS->value => TotalUploadsQueryResult::class,
     ]
 )]
 interface QueryResultInterface
