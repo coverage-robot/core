b1dcdd51be0f6df45897df6f816e5ada7bfd5030
Ryan Maber <ryan@ryanmaber.com>
2023-10-27 02:20:32 +0100

    Fix deserialization for individual coverage objects which inherit a base class in the discriminator map
    Signed-off-by: Ryan Maber <ryan@ryanmaber.com>

--- services/analyse/src/Query/Result/QueryResultInterface.php
+++ services/analyse/src/Query/Result/QueryResultInterface.php
@@ -10,7 +10,6 @@ use Symfony\Component\Serializer\Annotation\DiscriminatorMap;
     [
         QueryResult::COMMIT_COLLECTION->value => CommitCollectionQueryResult::class,
         QueryResult::COMMIT->value => CommitQueryResult::class,
-        QueryResult::COVERAGE->value => CoverageQueryResult::class,
         QueryResult::FILE_COVERAGE_COLLECTION->value => FileCoverageCollectionQueryResult::class,
         QueryResult::FILE_COVERAGE->value => FileCoverageQueryResult::class,
         QueryResult::LINE_COVERAGE_COLLECTION->value => LineCoverageCollectionQueryResult::class,
@@ -18,6 +17,23 @@ use Symfony\Component\Serializer\Annotation\DiscriminatorMap;
         QueryResult::TAG_COVERAGE_COLLECTION->value => TagCoverageCollectionQueryResult::class,
         QueryResult::TAG_COVERAGE->value => TagCoverageQueryResult::class,
         QueryResult::TOTAL_UPLOADS->value => TotalUploadsQueryResult::class,
+
+        /**
+         * The ordering of this map is **very** important, as this dictates what the serializer will output for the type
+         * property when conversed to JSON. And, by extension, this means it will impact how the JSON values are deserialized
+         * back into classes.
+         *
+         * Specifically, because the coverage result base class is extended in other classes, it **must** go after them in the
+         * discriminator map, as otherwise the type will be incorrectly serialized as "COVERAGE" instead of the
+         * correct type for the child class.
+         *
+         * In practice, Symfony will iterate through the map in order, and the first class which is implemented by the
+         * object being serialized will be used to convert the type back into the discriminator value (this includes inherited classes).
+         *
+         * @see TagCoverageQueryResult
+         * @see FileCoverageQueryResult
+         */
+        QueryResult::COVERAGE->value => CoverageQueryResult::class,
     ]
 )]
 interface QueryResultInterface
