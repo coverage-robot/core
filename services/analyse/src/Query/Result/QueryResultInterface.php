19ad84d85d1767970e78b19371a987cf17174d49
Ryan Maber <ryan@ryanmaber.com>
2023-10-28 17:16:06 +0100

    Tag availability query tests
    Signed-off-by: Ryan Maber <ryan@ryanmaber.com>

--- services/analyse/src/Query/Result/QueryResultInterface.php
+++ services/analyse/src/Query/Result/QueryResultInterface.php
@@ -8,14 +8,15 @@ use Symfony\Component\Serializer\Annotation\DiscriminatorMap;
 #[DiscriminatorMap(
     'type',
     [
-        QueryResult::COMMIT_COLLECTION->value => CommitCollectionQueryResult::class,
-        QueryResult::COMMIT->value => CommitQueryResult::class,
         QueryResult::FILE_COVERAGE_COLLECTION->value => FileCoverageCollectionQueryResult::class,
         QueryResult::FILE_COVERAGE->value => FileCoverageQueryResult::class,
+
         QueryResult::LINE_COVERAGE_COLLECTION->value => LineCoverageCollectionQueryResult::class,
         QueryResult::LINE_COVERAGE->value => LineCoverageQueryResult::class,
+
         QueryResult::TAG_COVERAGE_COLLECTION->value => TagCoverageCollectionQueryResult::class,
         QueryResult::TAG_COVERAGE->value => TagCoverageQueryResult::class,
+
         QueryResult::TOTAL_UPLOADS->value => TotalUploadsQueryResult::class,
         QueryResult::TAG_AVAILABILITY->value => TagAvailabilityQueryResult::class,
 
