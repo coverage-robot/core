aaa1f3a2b2e28f0a6de1fdc2533c7c7e1ca7f974
Ryan Maber <ryan@ryanmaber.com>
2023-10-31 01:16:42 +0000

    Remove delay on webhook handling to see if that improves premature analysis
    Signed-off-by: Ryan Maber <ryan@ryanmaber.com>

--- infrastructure/queue/main.tf
+++ infrastructure/queue/main.tf
@@ -22,10 +22,4 @@ resource "aws_sqs_queue" "webhooks_queue" {
   # The queue is going to spend a fair amount of time providing empty receives, so switching
   # to long polling will reduce the number of requests made to SQS.
   receive_wait_time_seconds = 20
-
-  # Add a small (5 second) delay between webhooks received from providers and analysis on
-  # coverage starting. This gives a short period of time for providers to start more jobs
-  # for the same commit. The queue is a first-in first-out, so the delay cannot be added
-  # on a per-message basis.
-  delay_seconds = 5
 }
