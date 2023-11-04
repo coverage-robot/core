bcfa9104d24ccdc322fb6e7b5239ef3af7302109
Ryan Maber <ryan@ryanmaber.com>
2023-11-04 23:15:56 +0000

    Make sure to serialize event so that models can be transmitted in json
    Signed-off-by: Ryan Maber <ryan@ryanmaber.com>

--- packages/event/src/Model/IngestSuccess.php
+++ packages/event/src/Model/IngestSuccess.php
@@ -5,6 +5,7 @@ namespace Packages\Event\Model;
 use DateTimeImmutable;
 use Packages\Event\Enum\Event;
 use Packages\Models\Enum\Provider;
+use Symfony\Component\Serializer\Annotation\Ignore;
 
 class IngestSuccess implements EventInterface
 {
@@ -13,41 +14,54 @@ class IngestSuccess implements EventInterface
     ) {
     }
 
+    public function getUpload(): EventInterface
+    {
+        return $this->upload;
+    }
+
+    #[Ignore]
     public function getProvider(): Provider
     {
         return $this->upload->getProvider();
     }
 
+    #[Ignore]
     public function getOwner(): string
     {
         return $this->upload->getOwner();
     }
 
+    #[Ignore]
     public function getRepository(): string
     {
         return $this->upload->getRepository();
     }
 
+    #[Ignore]
     public function getCommit(): string
     {
         return $this->upload->getCommit();
     }
 
+    #[Ignore]
     public function getPullRequest(): int|string|null
     {
         return $this->upload->getPullRequest();
     }
 
+    #[Ignore]
     public function getRef(): string
     {
         return $this->upload->getRef();
     }
 
+    #[Ignore]
     public function getType(): Event
     {
         return Event::INGEST_SUCCESS;
     }
 
+    #[Ignore]
     public function getEventTime(): DateTimeImmutable
     {
         return $this->upload->getIngestTime();
