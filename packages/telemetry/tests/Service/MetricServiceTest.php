b75dc733853c2a0ce6e1678f51d702fa33dfbd46
Ryan Maber <ryan@ryanmaber.com>
2023-11-20 00:43:38 +0000

    Add tests to metric serialization
    Signed-off-by: Ryan Maber <ryan@ryanmaber.com>

---
+++ packages/telemetry/tests/Service/MetricServiceTest.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace Packages\Telemetry\Tests\Service;
+
+use DateTimeImmutable;
+use EnricoStahn\JsonAssert\Assert;
+use Packages\Contracts\Environment\Environment;
+use Packages\Telemetry\Enum\EnvironmentVariable;
+use Packages\Telemetry\Enum\Resolution;
+use Packages\Telemetry\Enum\Unit;
+use Packages\Telemetry\Service\MetricService;
+use Packages\Telemetry\Tests\Mock\Factory\MockEnvironmentServiceFactory;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\MockObject\Exception;
+use PHPUnit\Framework\TestCase;
+use Psr\Clock\ClockInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
+use Symfony\Component\Serializer\Mapping\Loader\AnnotationLoader;
+use Symfony\Component\Serializer\NameConverter\CamelCaseToSnakeCaseNameConverter;
+use Symfony\Component\Serializer\NameConverter\MetadataAwareNameConverter;
+use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
+use Symfony\Component\Serializer\Normalizer\BackedEnumNormalizer;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Normalizer\UidNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+class MetricServiceTest extends TestCase
+{
+    use Assert;
+
+    /**
+     * @param (float|int)[]|float|int $value
+     * @param string[][] $dimensions
+     * @param array<string, (float|int)[]|float|int> $properties
+     *
+     * @throws Exception
+     */
+    #[DataProvider('putMetricDataProvider')]
+    public function testMetricBuilding(
+        string $metric,
+        array|float|int $value,
+        Unit $unit,
+        Resolution $resolution,
+        array $dimensions,
+        array $properties
+    ): void {
+        $mockMetricsLogger = $this->createMock(LoggerInterface::class);
+        $mockClock = $this->createMock(ClockInterface::class);
+        $mockClock->method('now')
+            ->willReturn(new DateTimeImmutable('2023-11-20 09:00:00'));
+
+        $metricService = new MetricService(
+            $mockMetricsLogger,
+            $mockClock,
+            MockEnvironmentServiceFactory::getMock(
+                $this,
+                Environment::DEVELOPMENT,
+                [
+                    EnvironmentVariable::AWS_LAMBDA_FUNCTION_NAME->value => 'mock-function-name',
+                    EnvironmentVariable::AWS_LAMBDA_FUNCTION_VERSION->value => '$LATEST'
+                ]
+            ),
+            new Serializer(
+                [
+                    new ArrayDenormalizer(),
+                    new UidNormalizer(),
+                    new BackedEnumNormalizer(),
+                    new DateTimeNormalizer(),
+                    new ObjectNormalizer(
+                        classMetadataFactory: new ClassMetadataFactory(
+                            new AnnotationLoader()
+                        ),
+                        nameConverter: new MetadataAwareNameConverter(
+                            new ClassMetadataFactory(
+                                new AnnotationLoader()
+                            ),
+                            new CamelCaseToSnakeCaseNameConverter()
+                        ),
+                    ),
+                ],
+                [new JsonEncoder()]
+            )
+        );
+
+        $mockMetricsLogger->expects($this->once())
+            ->method('info')
+            ->with(
+                self::callback(function (string $serialisedMetric) {
+                    $json = json_decode($serialisedMetric);
+
+                    $this->assertJsonMatchesSchema(
+                        $json,
+                        './tests/Service/cloudwatch-emf-schema.json'
+                    );
+                    $this->assertJsonValueEquals(
+                        'mock-function-name',
+                        MetricService::FUNCTION_NAME,
+                        $json
+                    );
+                    $this->assertJsonValueEquals(
+                        '$LATEST',
+                        MetricService::FUNCTION_VERSION,
+                        $json
+                    );
+
+                    return true;
+                })
+            );
+
+        $metricService->put(
+            $metric,
+            $value,
+            $unit,
+            $resolution,
+            $dimensions,
+            $properties
+        );
+    }
+
+    public static function putMetricDataProvider(): array
+    {
+        return [
+            [
+                'mock-metric',
+                1,
+                Unit::COUNT,
+                Resolution::LOW,
+                [],
+                []
+            ],
+            [
+                'mock-metric-2',
+                1000,
+                Unit::MILLISECONDS,
+                Resolution::HIGH,
+                [
+                    ['owner'],
+                    ['owner', 'repository']
+                ],
+                [
+                    'owner' => 1,
+                    'repository' => 2
+                ]
+            ]
+        ];
+    }
+}
