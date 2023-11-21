20386eedae54552811eb26bd792f1f8ef192c4f4
Ryan Maber <ryan@ryanmaber.com>
2023-11-21 19:27:10 +0000

    API sentry instrumentation
    Signed-off-by: Ryan Maber <ryan@ryanmaber.com>

--- services/api/config/bundles.php
+++ services/api/config/bundles.php
@@ -7,4 +7,5 @@ return [
     AsyncAws\Symfony\Bundle\AsyncAwsBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['prod' => true],
 ];
