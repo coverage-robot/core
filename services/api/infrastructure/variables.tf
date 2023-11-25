f59dd6ab739307728054a79238c7a5f4a1838349
Ryan Maber <ryan@ryanmaber.com>
2023-11-25 13:01:23 +0000

    Run the api on php 8.2 until the gd layer is available for the 8.3 runtime
    Signed-off-by: Ryan Maber <ryan@ryanmaber.com>

--- services/api/infrastructure/variables.tf
+++ services/api/infrastructure/variables.tf
@@ -10,5 +10,9 @@ variable "environment" {
 
 variable "php_version" {
   type    = string
-  default = "php-83"
+
+  # The Bref GD layer isn't currently available on the PHP 8.3 runtime, so we're leaving this as 8.2
+  # for now. Tests, linting and all other checks pass with 8.3 currently, so in the near future this
+  # will need to be bumped.
+  default = "php-82"
 }
