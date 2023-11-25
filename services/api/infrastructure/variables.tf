53ac514ddc4844b8f73fddf444677fced82a1503
Ryan Maber <ryan@ryanmaber.com>
2023-11-25 13:04:35 +0000

    Linting
    Signed-off-by: Ryan Maber <ryan@ryanmaber.com>

--- services/api/infrastructure/variables.tf
+++ services/api/infrastructure/variables.tf
@@ -9,7 +9,7 @@ variable "environment" {
 }
 
 variable "php_version" {
-  type    = string
+  type = string
 
   # The Bref GD layer isn't currently available on the PHP 8.3 runtime, so we're leaving this as 8.2
   # for now. Tests, linting and all other checks pass with 8.3 currently, so in the near future this
