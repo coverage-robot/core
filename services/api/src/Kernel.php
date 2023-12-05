d4518d1b622cb814c85025861a59bdf7f018dd5d
Ryan Maber <ryan@ryanmaber.com>
2023-12-05 21:22:04 +0000

    Make the twig cache writable at runtime
    Signed-off-by: Ryan Maber <ryan@ryanmaber.com>

--- services/api/src/Kernel.php
+++ services/api/src/Kernel.php
@@ -8,4 +8,17 @@ use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 class Kernel extends BrefKernel
 {
     use MicroKernelTrait;
+
+    public function getWritableCacheDirectories() : array{
+        return [
+            ...parent::getWritableCacheDirectories(),
+            /**
+             * The twig cache is going to be used at runtime to store the
+             * compiled templates. If we don't allow this directory to be writable
+             * (i.e. symlink it), then the routes which render twig templates will
+             * fail at runtime.
+             */
+            'twig'
+        ];
+    }
 }
