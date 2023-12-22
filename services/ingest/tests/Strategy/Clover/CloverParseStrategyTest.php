13b624715526a31fb06f3c7df3c49a2259902483
Ryan Maber <ryan@ryanmaber.com>
2023-12-22 00:24:52 +0000

    Use snapshots to test ingest output
    Signed-off-by: Ryan Maber <ryan@ryanmaber.com>

--- services/ingest/tests/Strategy/Clover/CloverParseStrategyTest.php
+++ services/ingest/tests/Strategy/Clover/CloverParseStrategyTest.php
@@ -2,38 +2,41 @@
 
 namespace App\Tests\Strategy\Clover;
 
+use App\Service\PathFixingService;
 use App\Strategy\Clover\CloverParseStrategy;
 use App\Strategy\ParseStrategyInterface;
 use App\Tests\Strategy\AbstractParseStrategyTestCase;
+use Psr\Log\NullLogger;
 
 class CloverParseStrategyTest extends AbstractParseStrategyTestCase
 {
     public static function coverageFilesDataProvider(): iterable
     {
-        yield from parent::parseCoverageFixtures(__DIR__ . '/../../Fixture/Clover', 'xml');
+        yield from parent::parseCoverageFixtures(__DIR__ . '/../../Fixture/Clover');
 
         yield 'Does not handle invalid file' => [
             'mock/project/root',
             'invalid-file-content',
-            false,
-            ''
+            false
         ];
 
         yield 'Does not handle invalid schema file' => [
             'mock/project/root',
             <<<XML
-                <?xml version="1.0" encoding="UTF-8"?>
-                <coverage generatedAt="abc" clover="-99">
-                  <project timestamp="1686599618433" name="All files"></project>
-                </coverage>
-                XML,
-            false,
-            ''
+            <?xml version="1.0" encoding="UTF-8"?>
+            <coverage generatedAt="abc" clover="-99">
+              <project timestamp="1686599618433" name="All files"></project>
+            </coverage>
+            XML,
+            false
         ];
     }
 
     protected function getParserStrategy(): ParseStrategyInterface
     {
-        return $this->getContainer()->get(CloverParseStrategy::class);
+        return new CloverParseStrategy(
+            new NullLogger(),
+            new PathFixingService()
+        );
     }
 }
--- services/ingest/tests/Strategy/Lcov/LcovParseStrategyTest.php
+++ services/ingest/tests/Strategy/Lcov/LcovParseStrategyTest.php
@@ -2,26 +2,30 @@
 
 namespace App\Tests\Strategy\Lcov;
 
+use App\Service\PathFixingService;
 use App\Strategy\Lcov\LcovParseStrategy;
 use App\Strategy\ParseStrategyInterface;
 use App\Tests\Strategy\AbstractParseStrategyTestCase;
+use Psr\Log\NullLogger;
 
 class LcovParseStrategyTest extends AbstractParseStrategyTestCase
 {
     public static function coverageFilesDataProvider(): iterable
     {
-        yield from parent::parseCoverageFixtures(__DIR__ . '/../../Fixture/Lcov', 'info');
+        yield from parent::parseCoverageFixtures(__DIR__ . '/../../Fixture/Lcov');
 
         yield 'Does not handle invalid file' => [
             'mock/project/root',
             'invalid-file-content',
-            false,
-            ''
+            false
         ];
     }
 
     protected function getParserStrategy(): ParseStrategyInterface
     {
-        return $this->getContainer()->get(LcovParseStrategy::class);
+        return new LcovParseStrategy(
+            new NullLogger(),
+            new PathFixingService()
+        );
     }
 }
