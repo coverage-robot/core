9b426356240b533500f11f8fa5217c7b303aa7fc
Ryan Maber <ryan@ryanmaber.com>
2024-02-18 19:48:45 +0000

    Linting
    Signed-off-by: Ryan Maber <ryan@ryanmaber.com>

--- services/publish/tests/Service/Publisher/AbstractPublisherServiceTestCase.php
+++ services/publish/tests/Service/Publisher/AbstractPublisherServiceTestCase.php
@@ -7,6 +7,9 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 abstract class AbstractPublisherServiceTestCase extends KernelTestCase
 {
+    /**
+     * Test that the publisher supports the correct events.
+     */
     abstract public function testSupports(EventInterface $event, bool $expectedSupport): void;
 
     abstract public static function supportsDataProvider(): array;
