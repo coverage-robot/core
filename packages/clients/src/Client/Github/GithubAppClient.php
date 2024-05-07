3458fc48066665516d4d489168b4453904cf1f50
Ryan Maber <ryan@ryanmaber.com>
2024-05-07 18:44:34 +0100

    Allow constructor to occur first

--- packages/clients/src/Client/Github/GithubAppClient.php
+++ packages/clients/src/Client/Github/GithubAppClient.php
@@ -32,6 +32,8 @@ class GithubAppClient extends Client
         ?string $apiVersion = null,
         public readonly ?string $enterpriseUrl = null
     ) {
+        parent::__construct($httpClientBuilder, $apiVersion, $this->enterpriseUrl);
+
         /**
          * Retry requests up to 2 additional times when a failure occurs.
          *
@@ -39,13 +41,12 @@ class GithubAppClient extends Client
          * retrieve commit history. In this case, it's safe to retry the request and see if we can get a
          * response 1 or 2 more times.
          */
-        $httpClientBuilder->addPlugin(
-            new RetryPlugin([
-                'retries' => 2
-            ])
-        );
-
-        parent::__construct($httpClientBuilder, $apiVersion, $this->enterpriseUrl);
+        $this->getHttpClientBuilder()
+            ->addPlugin(
+                new RetryPlugin([
+                    'retries' => 2
+                ])
+            );
     }
 
     /**
