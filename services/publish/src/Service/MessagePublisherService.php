27bf4c99d6a0629af53dd5e63b5fe023823e82ab
Ryan Maber <ryan@ryanmaber.com>
2024-07-21 20:57:32 +0100

    tidy annotations

--- services/publish/src/Service/MessagePublisherService.php
+++ services/publish/src/Service/MessagePublisherService.php
@@ -12,8 +12,16 @@ use Symfony\Component\DependencyInjection\Attribute\AutowireIterator;
 
 final class MessagePublisherService implements MessagePublisherServiceInterface
 {
+    /**
+     * @param iterable<int, MessagePublisherService> $publishers
+     */
     public function __construct(
-        #[AutowireIterator('app.publisher_service', defaultPriorityMethod: 'getPriority')]
+        #[
+            AutowireIterator(
+                "app.publisher_service",
+                defaultPriorityMethod: "getPriority"
+            )
+        ]
         private readonly iterable $publishers,
         private readonly LoggerInterface $publisherServiceLogger,
         private readonly MetricServiceInterface $metricService
@@ -24,16 +32,17 @@ final class MessagePublisherService implements MessagePublisherServiceInterface
      * Publish the message with _all_ publishers which support it.
      */
     #[Override]
-    public function publish(PublishableMessageInterface $publishableMessage): bool
-    {
+    public function publish(
+        PublishableMessageInterface $publishableMessage
+    ): bool {
         $successful = true;
 
         foreach ($this->publishers as $publisher) {
             if (!$publisher instanceof PublisherServiceInterface) {
                 $this->publisherServiceLogger->critical(
-                    'Publisher does not implement the correct interface.',
+                    "Publisher does not implement the correct interface.",
                     [
-                        'persistService' => $publisher::class
+                        "persistService" => $publisher::class,
                     ]
                 );
 
@@ -42,8 +51,8 @@ final class MessagePublisherService implements MessagePublisherServiceInterface
 
             $this->publisherServiceLogger->info(
                 sprintf(
-                    'Publishing %s using %s',
-                    (string)$publishableMessage,
+                    "Publishing %s using %s",
+                    (string) $publishableMessage,
                     $publisher::class
                 )
             );
@@ -51,9 +60,9 @@ final class MessagePublisherService implements MessagePublisherServiceInterface
             if (!$publisher->supports($publishableMessage)) {
                 $this->publisherServiceLogger->info(
                     sprintf(
-                        'Not publishing using %s, as it does not support %s',
+                        "Not publishing using %s, as it does not support %s",
                         $publisher::class,
-                        (string)$publishableMessage,
+                        (string) $publishableMessage
                     )
                 );
 
@@ -64,18 +73,13 @@ final class MessagePublisherService implements MessagePublisherServiceInterface
 
             if ($publishSucceeded) {
                 $this->metricService->put(
-                    metric: 'PublishedResults',
+                    metric: "PublishedResults",
                     value: 1,
                     unit: Unit::COUNT,
-                    dimensions: [
-                        ['owner'],
-                        ['owner', 'type']
-                    ],
+                    dimensions: [["owner"], ["owner", "type"]],
                     properties: [
-                        'owner' => $publishableMessage->getEvent()
-                            ?->getOwner(),
-                        'type' => $publishableMessage->getType()
-                            ->value
+                        "owner" => $publishableMessage->getEvent()?->getOwner(),
+                        "type" => $publishableMessage->getType()->value,
                     ]
                 );
             }
@@ -84,9 +88,9 @@ final class MessagePublisherService implements MessagePublisherServiceInterface
 
             $this->publisherServiceLogger->info(
                 sprintf(
-                    'Publishing using %s continues to be a %s',
+                    "Publishing using %s continues to be a %s",
                     $publisher::class,
-                    $successful ? 'success' : 'fail'
+                    $successful ? "success" : "fail"
                 )
             );
         }
