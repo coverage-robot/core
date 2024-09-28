fc3d9f3e4341745db9f52717fac7ea97677999bf
Ryan Maber <ryan@ryanmaber.com>
2024-09-28 17:53:44 +0100

    Remove old event source argument with the environment service method

--- packages/event/src/Client/EventBusClientInterface.php
+++ packages/event/src/Client/EventBusClientInterface.php
@@ -4,14 +4,12 @@ namespace Packages\Event\Client;
 
 use DateTimeInterface;
 use Packages\Contracts\Event\EventInterface;
-use Packages\Contracts\Event\EventSource;
 
 interface EventBusClientInterface
 {
-    public function fireEvent(EventSource $source, EventInterface $event): bool;
+    public function fireEvent(EventInterface $event): bool;
 
     public function scheduleEvent(
-        EventSource $source,
         EventInterface $event,
         DateTimeInterface $fireAt
     ): bool;
