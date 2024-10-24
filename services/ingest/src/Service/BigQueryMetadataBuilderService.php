376b0b9e34619d9847280aa837f1a5c7fb50ae98
Ryan Maber <ryan@ryanmaber.com>
2024-10-25 00:02:50 +0100

    Push upload id into bigquery

--- services/ingest/src/Service/BigQueryMetadataBuilderService.php
+++ services/ingest/src/Service/BigQueryMetadataBuilderService.php
@@ -61,6 +61,7 @@ final class BigQueryMetadataBuilderService
             'uploadId' => $upload->getUploadId(),
             'ingestTime' => $upload->getEventTime()->format('Y-m-d H:i:s'),
             'provider' => $upload->getProvider(),
+            'projectId' => $upload->getProjectId(),
             'owner' => $upload->getOwner(),
             'repository' => $upload->getRepository(),
             'commit' => $upload->getCommit(),
