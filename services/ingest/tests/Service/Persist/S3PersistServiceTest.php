536021dc89326950bb5d8d517bdda6a6544d46ce
Ryan Maber <ryan@ryanmaber.com>
2024-10-24 01:32:25 +0100

    Fix missing test

--- services/ingest/tests/Service/Persist/S3PersistServiceTest.php
+++ services/ingest/tests/Service/Persist/S3PersistServiceTest.php
@@ -42,6 +42,7 @@ final class S3PersistServiceTest extends KernelTestCase
             'parent' => json_encode($upload->getParent(), JSON_THROW_ON_ERROR),
             'uploadId' => $upload->getUploadId(),
             'provider' => $upload->getProvider()->value,
+            'projectId' => null,
             'owner' => $upload->getOwner(),
             'repository' => $upload->getRepository(),
             'ref' => $upload->getRef(),
