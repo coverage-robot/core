bbcb783e2c18a85de98b0e04acb0980a3fb8a22b
Ryan Maber <ryan@ryanmaber.com>
2025-02-16 03:42:06 +0000

    Add better handling for query result iterators which are serialized and deserialized from cache

--- services/analyse/tests/Client/DynamoDbClientTest.php
+++ services/analyse/tests/Client/DynamoDbClientTest.php
@@ -7,9 +7,9 @@ namespace App\Tests\Client;
 use App\Client\DynamoDbClient;
 use App\Enum\EnvironmentVariable;
 use App\Query\Result\QueryResultInterface;
+use App\Query\Result\QueryResultIterator;
 use App\Query\Result\TotalCoverageQueryResult;
-use App\Query\Result\TotalUploadsQueryResult;
-use App\Tests\Mock\Factory\MockSerializerFactory;
+use ArrayIterator;
 use AsyncAws\Core\Response;
 use AsyncAws\DynamoDb\Input\GetItemInput;
 use AsyncAws\DynamoDb\Result\GetItemOutput;
@@ -18,12 +18,13 @@ use InvalidArgumentException;
 use Packages\Configuration\Mock\MockEnvironmentServiceFactory;
 use Packages\Contracts\Environment\Environment;
 use Packages\Contracts\Environment\Service;
-use PHPUnit\Framework\TestCase;
 use Psr\Log\NullLogger;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Contracts\HttpClient\ResponseInterface;
 
-final class DynamoDbClientTest extends TestCase
+final class DynamoDbClientTest extends KernelTestCase
 {
     public function testPutQueryResultInCache(): void
     {
@@ -31,7 +32,7 @@ final class DynamoDbClientTest extends TestCase
         $mockResponse->method('getInfo')
             ->willReturn(\Symfony\Component\HttpFoundation\Response::HTTP_OK);
 
-        $queryResult = new TotalCoverageQueryResult(0, 0, 0, 0, 0);
+        $queryResult = new TotalCoverageQueryResult(1.2, 0, 0, 0, 0);
 
         $mockClient = $this->createMock(\AsyncAws\DynamoDb\DynamoDbClient::class);
 
@@ -45,7 +46,19 @@ final class DynamoDbClientTest extends TestCase
                             $parameters['TableName']
                         );
                         $this->assertSame('mock-cache-key', $parameters['Item']['cacheKey']['S']);
-                        $this->assertSame('mock-serialized-result', $parameters['Item']['result']['S']);
+                        $this->assertSame(
+                            json_encode(
+                                [
+                                    'coveragePercentage' => 1.2,
+                                    'lines' => 0,
+                                    'covered' => 0,
+                                    'partial' => 0,
+                                    'uncovered' => 0,
+                                    'type' => 'COVERAGE'
+                                ]
+                            ),
+                            $parameters['Item']['result']['S']
+                        );
                         $this->assertIsNumeric($parameters['Item']['expiry']['N']);
                         return true;
                     }
@@ -70,17 +83,7 @@ final class DynamoDbClientTest extends TestCase
                     EnvironmentVariable::QUERY_CACHE_TABLE_NAME->value => 'mock-query-cache-table-name'
                 ]
             ),
-            MockSerializerFactory::getMock(
-                $this,
-                serializeMap: [
-                    [
-                        $queryResult,
-                        'json',
-                        [],
-                        'mock-serialized-result'
-                    ]
-                ]
-            ),
+            $this->getContainer()->get(SerializerInterface::class),
             new NullLogger()
         );
 
@@ -100,14 +103,29 @@ final class DynamoDbClientTest extends TestCase
             ->willReturn([
                 'Item' => [
                     'result' => [
-                        'S' => 'mock-serialized-result'
+                        'S' => json_encode(
+                            [
+                                'coveragePercentage' => 1.1,
+                                'lines' => 0,
+                                'covered' => 0,
+                                'partial' => 0,
+                                'uncovered' => 0,
+                                'type' => 'COVERAGE'
+                            ]
+                        )
                     ]
                 ]
             ]);
         $mockResponse->method('getInfo')
             ->willReturn(\Symfony\Component\HttpFoundation\Response::HTTP_OK);
 
-        $queryResult = new TotalUploadsQueryResult([], [], []);
+        $queryResult = new TotalCoverageQueryResult(
+            1.1,
+            0,
+            0,
+            0,
+            0
+        );
 
         $mockClient = $this->createMock(\AsyncAws\DynamoDb\DynamoDbClient::class);
 
@@ -144,18 +162,7 @@ final class DynamoDbClientTest extends TestCase
                     EnvironmentVariable::QUERY_CACHE_TABLE_NAME->value => 'mock-query-cache-table-name'
                 ]
             ),
-            MockSerializerFactory::getMock(
-                $this,
-                deserializeMap: [
-                    [
-                        'mock-serialized-result',
-                        QueryResultInterface::class,
-                        'json',
-                        [],
-                        $queryResult
-                    ]
-                ]
-            ),
+            $this->getContainer()->get(SerializerInterface::class),
             new NullLogger()
         );
 
@@ -164,6 +171,89 @@ final class DynamoDbClientTest extends TestCase
             $dynamoDbClient->tryFromQueryCache(
                 'mock-cache-key'
             )
+        );
+    }
+
+    public function testTryIteratorFromQueryCache(): void
+    {
+        $mockResponse = $this->createMock(ResponseInterface::class);
+        $mockResponse->expects($this->once())
+            ->method('toArray')
+            ->willReturn([
+                'Item' => [
+                    'result' => [
+                        'S' => json_encode([
+                            [
+                                'coveragePercentage' => 1.1,
+                                'lines' => 0,
+                                'covered' => 0,
+                                'partial' => 0,
+                                'uncovered' => 0,
+                                'type' => 'COVERAGE'
+                            ]
+                        ])
+                    ]
+                ]
+            ]);
+        $mockResponse->method('getInfo')
+            ->willReturn(\Symfony\Component\HttpFoundation\Response::HTTP_OK);
+
+        $mockClient = $this->createMock(\AsyncAws\DynamoDb\DynamoDbClient::class);
+
+        $mockClient->expects($this->once())
+            ->method('getItem')
+            ->with(
+                self::callback(
+                    function (GetItemInput $input): bool {
+                        $this->assertSame(
+                            'mock-query-cache-table-name',
+                            $input->getTableName()
+                        );
+                        $this->assertSame('mock-cache-key', $input->getKey()['cacheKey']->getS());
+                        return true;
+                    }
+                )
+            )
+            ->willReturn(
+                new GetItemOutput(
+                    new Response(
+                        $mockResponse,
+                        $this->createMock(HttpClientInterface::class),
+                        new NullLogger()
+                    )
+                )
+            );
+
+        $dynamoDbClient = new DynamoDbClient(
+            $mockClient,
+            MockEnvironmentServiceFactory::createMock(
+                Environment::TESTING,
+                Service::ANALYSE,
+                [
+                    EnvironmentVariable::QUERY_CACHE_TABLE_NAME->value => 'mock-query-cache-table-name'
+                ]
+            ),
+            $this->getContainer()->get(SerializerInterface::class),
+            new NullLogger()
+        );
+
+        $result = $dynamoDbClient->tryFromQueryCache('mock-cache-key');
+
+        $this->assertInstanceOf(QueryResultIterator::class, $result);
+
+        $this->assertEquals(
+            1,
+            $result->count()
+        );
+        $this->assertEquals(
+            new TotalCoverageQueryResult(
+                1.1,
+                0,
+                0,
+                0,
+                0
+            ),
+            $result->current()
         );
     }
 
@@ -188,7 +278,7 @@ final class DynamoDbClientTest extends TestCase
                     EnvironmentVariable::QUERY_CACHE_TABLE_NAME->value => 'mock-query-cache-table-name'
                 ]
             ),
-            MockSerializerFactory::getMock($this),
+            $this->getContainer()->get(SerializerInterface::class),
             new NullLogger()
         );
 
@@ -197,6 +287,83 @@ final class DynamoDbClientTest extends TestCase
         $dynamoDbClient->putQueryResultInCache(
             'mock-cache-key',
             $queryResult
+        );
+    }
+
+    public function testPutQueryResultIteratorInCache(): void
+    {
+        $mockResponse = $this->createMock(ResponseInterface::class);
+        $mockResponse->method('getInfo')
+            ->willReturn(\Symfony\Component\HttpFoundation\Response::HTTP_OK);
+
+        $queryResult = new QueryResultIterator(
+            new ArrayIterator([
+                new TotalCoverageQueryResult(1.1, 0, 0, 0, 0)
+            ]),
+            1,
+            static fn(QueryResultInterface $row): QueryResultInterface => $row
+        );
+
+        $mockClient = $this->createMock(\AsyncAws\DynamoDb\DynamoDbClient::class);
+
+        $mockClient->expects($this->once())
+            ->method('putItem')
+            ->with(
+                self::callback(
+                    function (array $parameters): bool {
+                        $this->assertSame(
+                            'mock-query-cache-table-name',
+                            $parameters['TableName']
+                        );
+                        $this->assertSame('mock-cache-key', $parameters['Item']['cacheKey']['S']);
+                        $this->assertSame(
+                            json_encode(
+                                [
+                                    [
+                                        'coveragePercentage' => 1.1,
+                                        'lines' => 0,
+                                        'covered' => 0,
+                                        'partial' => 0,
+                                        'uncovered' => 0,
+                                        'type' => 'COVERAGE'
+                                    ]
+                                ]
+                            ),
+                            $parameters['Item']['result']['S']
+                        );
+                        $this->assertIsNumeric($parameters['Item']['expiry']['N']);
+                        return true;
+                    }
+                )
+            )
+            ->willReturn(
+                new PutItemOutput(
+                    new Response(
+                        $mockResponse,
+                        $this->createMock(HttpClientInterface::class),
+                        new NullLogger()
+                    )
+                )
+            );
+
+        $dynamoDbClient = new DynamoDbClient(
+            $mockClient,
+            MockEnvironmentServiceFactory::createMock(
+                Environment::TESTING,
+                Service::ANALYSE,
+                [
+                    EnvironmentVariable::QUERY_CACHE_TABLE_NAME->value => 'mock-query-cache-table-name'
+                ]
+            ),
+            $this->getContainer()->get(SerializerInterface::class),
+            new NullLogger()
+        );
+
+        $this->assertTrue(
+            $dynamoDbClient->putQueryResultInCache(
+                'mock-cache-key',
+                $queryResult
+            )
         );
     }
 }
