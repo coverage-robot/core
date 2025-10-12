e863e6cdedcba3a7c620b4c61fd4a9fc9aac69f4
Ryan Maber <ryan@ryanmaber.com>
2025-10-13 00:37:03 +0100

    Remove output S3 bucket

--- infrastructure/bucket/outputs.tf
+++ infrastructure/bucket/outputs.tf
@@ -2,10 +2,6 @@ output "ingest_bucket" {
   value = aws_s3_bucket.coverage_ingest
 }
 
-output "output_bucket" {
-  value = aws_s3_bucket.coverage_output
-}
-
 output "object_reference_bucket" {
   value = aws_s3_bucket.service_object_references
 }
