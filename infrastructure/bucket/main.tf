7709e44435bda0bca805155ad44d046d32d1a087
Ryan Maber <ryan@ryanmaber.com>
2025-10-13 01:01:08 +0100

    Remove the S3 output bucket

--- infrastructure/bucket/main.tf
+++ infrastructure/bucket/main.tf
@@ -50,46 +50,6 @@ resource "aws_s3_bucket_lifecycle_configuration" "ingest_lifecycle" {
   ]
 }
 
-resource "aws_s3_bucket" "coverage_output" {
-  bucket = format("coverage-output-%s", var.environment)
-
-  tags = {
-    environment = var.environment
-  }
-}
-
-resource "aws_s3_bucket_lifecycle_configuration" "output_lifecycle" {
-  bucket = aws_s3_bucket.coverage_output.id
-
-  rule {
-    id = "delete-outputted-coverage-files"
-
-    filter {}
-
-    expiration {
-      # Delete the current version of objects after 7 days. These are the successfully
-      # ingested coverage files which have an associated output model for debugging
-      days = 7
-    }
-
-    noncurrent_version_expiration {
-      # Delete any objects holding a delete marker (or that are old version) after 1 day. This
-      # doesnt particularly happen with outputted models, as they're more for debugging purposes
-      noncurrent_days = 1
-    }
-
-    abort_incomplete_multipart_upload {
-      days_after_initiation = 1
-    }
-
-    status = "Enabled"
-  }
-
-  depends_on = [
-    aws_s3_bucket.coverage_output
-  ]
-}
-
 resource "aws_s3_bucket" "service_object_references" {
   bucket = format("coverage-object-reference-%s", var.environment)
 
